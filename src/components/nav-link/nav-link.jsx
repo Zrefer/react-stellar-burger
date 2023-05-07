4a354657a6149d414a0e81da04cc01fde673a0f8
Dmitry <81375514+Zrefer@users.noreply.github.com>
2023-05-07 15:55:22 +0300

    feat: add proptypes for NavLink

--- src/components/nav-link/nav-link.jsx
+++ src/components/nav-link/nav-link.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./nav-link.module.css";
+import PropTypes from "prop-types";
 
 class NavLink extends React.PureComponent {
-  static defaultProps = { active: false };
+  static defaultProps = { active: false, link: "#" };
 
   render() {
     return (
       <a
-        href={this.props.href}
+        href={this.props.link}
         className={`text_type_main-default ${styles["nav-link"]} ${
           !this.props.active &&
           `${styles["nav-link_inactive"]} text_color_inactive`
@@ -18,5 +19,10 @@ class NavLink extends React.PureComponent {
     );
   }
 }
+
+NavLink.propTypes = {
+  active: PropTypes.bool,
+  link: PropTypes.string,
+};
 
 export default NavLink;
